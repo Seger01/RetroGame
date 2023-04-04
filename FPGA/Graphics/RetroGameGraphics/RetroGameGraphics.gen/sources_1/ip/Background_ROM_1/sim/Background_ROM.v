3be44cdf536255659a5a69723817935fb485a717
Kampf <hmkamphof@gmail.com>
2023-04-04 11:57:36 +0200

    added withoud a tm z

--- FPGA/Graphics/RetroGameGraphics/RetroGameGraphics.gen/sources_1/ip/Background_ROM_1/sim/Background_ROM.v
+++ FPGA/Graphics/RetroGameGraphics/RetroGameGraphics.gen/sources_1/ip/Background_ROM_1/sim/Background_ROM.v
@@ -86,7 +86,7 @@ output wire [7 : 0] douta;
     .C_LOAD_INIT_FILE(1),
     .C_INIT_FILE_NAME("Background_ROM.mif"),
     .C_INIT_FILE("Background_ROM.mem"),
-    .C_USE_DEFAULT_DATA(0),
+    .C_USE_DEFAULT_DATA(1),
     .C_DEFAULT_DATA("0"),
     .C_HAS_RSTA(0),
     .C_RST_PRIORITY_A("CE"),
